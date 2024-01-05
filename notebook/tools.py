bf8a106a3d2d0ffce5a330212d84363fb8259f2d
WestonAnderson <weston@umd.edu>
2024-01-05 14:35:12 -0500

    Update FDW_PD_ValidateFnidName()
    The function will currently break if the data is at admin resolution 1 and the shapefile doesn't have blank admin level 2 fields (as some new ones don't). This update adds a check for the presence of admin 2 fields before trying to match admin names on that field

--- notebook/tools.py
+++ notebook/tools.py
@@ -277,7 +277,11 @@ def FDW_PD_ValidateFnidName(df, shape_used, shape_latest):
     '''Chekcing all FNID and Names between FDW data and FEWS NET Shapefiles for consistency.
     '''
     name_fdw = df[['fnid','admin_1','admin_2']].drop_duplicates()
-    name_shape = shape_used[['FNID','ADMIN1','ADMIN2']].drop_duplicates()
+    if 'ADMIN2' not in shape_used.columns: #admin 1 files may not have an admin 2 field, which would be entirely null
+        name_shape = shape_used[['FNID','ADMIN1']].drop_duplicates()
+    else:
+        name_shape = shape_used[['FNID','ADMIN1','ADMIN2']].drop_duplicates()
+        
     # Check all FNIDs exist in the shapefiles.
     fnid_not_in_shape = name_fdw[
         (name_fdw['fnid'].apply(lambda x: x[6] == 'A')) &
@@ -294,12 +298,13 @@ def FDW_PD_ValidateFnidName(df, shape_used, shape_latest):
         print('%s:\t"%s" (FDW) is changed to "%s" (shapefile).' % (FNID, admin_1, ADMIN1))
         df.loc[df['fnid'] == FNID, 'admin_1'] = ADMIN1
     # - Admin level 2
-    adm2 = name_merged['FNID'].apply(lambda x: x[6:8] == 'A2')
-    name_merged_adm2 = name_merged.loc[adm2, ['FNID','ADMIN2', 'admin_2']]
-    name_replace2 = name_merged_adm2[name_merged_adm2['ADMIN2'] != name_merged_adm2['admin_2']]
-    for i, (FNID, ADMIN2, admin_2) in name_replace2.iterrows():
-        print('%s:\t"%s" (FDW) is changed to "%s" (shapefile).' % (FNID, admin_2, ADMIN2))
-        df.loc[df['fnid'] == FNID, 'admin_2'] = ADMIN2
+    if 'ADMIN2' in shape_used.columns:
+        adm2 = name_merged['FNID'].apply(lambda x: x[6:8] == 'A2')
+        name_merged_adm2 = name_merged.loc[adm2, ['FNID','ADMIN2', 'admin_2']]
+        name_replace2 = name_merged_adm2[name_merged_adm2['ADMIN2'] != name_merged_adm2['admin_2']]
+        for i, (FNID, ADMIN2, admin_2) in name_replace2.iterrows():
+            print('%s:\t"%s" (FDW) is changed to "%s" (shapefile).' % (FNID, admin_2, ADMIN2))
+            df.loc[df['fnid'] == FNID, 'admin_2'] = ADMIN2
     # Define representative administrative names
     df.loc[df['fnid'].apply(lambda x: x[6:8] == 'A1'), 'name'] = df.loc[df['fnid'].apply(lambda x: x[6:8] == 'A1'), 'admin_1']
     df.loc[df['fnid'].apply(lambda x: x[6:8] == 'A2'), 'name'] = df.loc[df['fnid'].apply(lambda x: x[6:8] == 'A2'), 'admin_2']
@@ -311,7 +316,8 @@ def FDW_PD_ValidateFnidName(df, shape_used, shape_latest):
     df.loc[df['fnid'].apply(lambda x: x[6:8] == 'R4'), 'name'] = df.loc[df['fnid'].apply(lambda x: x[6:8] == 'R4'), 'admin_4']
     for gdf in [shape_used, shape_latest]:
         fdx = gdf.FNID.apply(lambda x: x[7] == '1'); gdf.loc[fdx,'name'] = gdf.loc[fdx, 'ADMIN1']
-        fdx = gdf.FNID.apply(lambda x: x[7] == '2'); gdf.loc[fdx,'name'] = gdf.loc[fdx, 'ADMIN2']
+        if 'ADMIN2' in shape_used.columns:
+            fdx = gdf.FNID.apply(lambda x: x[7] == '2'); gdf.loc[fdx,'name'] = gdf.loc[fdx, 'ADMIN2']
     return df
 
 
